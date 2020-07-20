8df9db075532459ec4c8e7bee311cd39184d301c
Adam Smolenski <adam.smolenski@gmail.com>
2020-07-20 19:40:35 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -15,7 +15,7 @@ def apply_coupons(cart, coupons)
         end
         discount[:count] = coupon[:num]
         discount[:item] = "#{coupon[:item]} W/COUPON"
-        discount[:price] = coupon[:cost]/coupn[:num]
+        discount[:price] = coupon[:cost]/coupon[:num]
         cart << discount
       end
     end
