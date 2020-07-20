f272bd3ce7d0d3e508f8f9d4977f70b87e5880f1
Adam Smolenski <adam.smolenski@gmail.com>
2020-07-20 20:42:07 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -57,9 +57,8 @@ def checkout(cart, coupons)
   # some irritated customers
   
   cart = consolidate_cart(cart)
-  
-  cart = apply_clearance(cart)
   cart = apply_coupons(cart,coupons)
+  cart = apply_clearance(cart)
   sum = add_up(cart)
   return sum 
 end
