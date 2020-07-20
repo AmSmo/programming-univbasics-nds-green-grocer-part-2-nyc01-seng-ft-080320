9493bfffed0edb41f2b9f69c5f77ca741005d278
Adam Smolenski <adam.smolenski@gmail.com>
2020-07-20 19:45:07 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -12,10 +12,10 @@ def apply_coupons(cart, coupons)
         redeem[:price] = coupon[:cost]/coupon[:num]
       end
     end
-    binding.pry
-    cart
+    
+    
   end
-  
+  cart
 end
 
 def apply_clearance(cart)
